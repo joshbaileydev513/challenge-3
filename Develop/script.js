b4118b00482e4736ef1f22172bcdf724c945fce1
Joshua Bailey <joshbaileydev513@gmail.com>
2023-02-11 16:07:16 -0500

    added new meta tags for SEO and psuedocoded the steps needed to complete the JS items

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// click generate -> presented w/ series of prompts -> 1. password length (8-128) 2. character types -> 
+// A. lowercase B. uppercase C. numeric D. Special Characters (need 1 of each)
+// 3. After all of that, generate password in alert of box on screen
