6651c9ba04d45289b9184163a57b9a01d70fe5a0
“[firstname <[joshbaileydev513@gmail.com]>
2023-02-13 18:40:31 -0500

    finsihed prompt sections and fixed some prompts being shown twice

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var pwLengthResult;
+var pwLowercaseResult;
+var pwUppercaseResult;
+var pwSpecialCharactersResult;
+var pwNumericalResult;
 
 // Write password to the #password input
 function writePassword() {
@@ -20,26 +25,79 @@ function generatePassword() {
     pwLength = parseInt(pwLength)
     console.log(pwLength)
     if (pwLength >= 8 && pwLength <= 128) {
-      // user has entered an acceptable password length between 8-128
+    // user has entered an acceptable password length between 8-128
+      pwLengthResult = pwLength;
     } else {
       window.alert("Please enter numerical values between 8-128")
+      generatePassword();
     }
   } else {
     window.alert("Please enter numerical values only")
+    generatePassword();
   }
 
-  // Character Type Section -lowercase,UPPERCASE,numeric, special characters
+  // Character Type Section -lowercase
   var lowercaseInput = window.prompt("Would you like to have lowercase characters in your password? (Y/N)");
-  if (confirmYesOrNo(lowercaseInput)) {
+  var yesOrNo = confirmYesOrNo(lowercaseInput);
+  if (yesOrNo === true) {
     // user wants lowercase
+    pwLowercaseResult = true;
+  } else if (yesOrNo === false) {
+    // user does not want lowercase
+    pwLowercaseResult = false;
+  } else {
+    // user did not choose y/n -prompted with message in window alert for confirmYesOrNO Function
+    generatePassword();
   }
+
+  // Character Type Section -UPPERCASE
+  var uppercaseInput = window.prompt("Would you like to have uppercase characters in your password? (Y/N)");
+  var yesOrNo = confirmYesOrNo(uppercaseInput);
+  if (yesOrNo === true) {
+    // user wants uppercase
+    pwUppercaseResult = true;
+  } else if (yesOrNo === false) {
+    // user does not want uppercase
+    pwUppercaseResult = false;
+  } else {
+    // user did not choose y/n -prompted with message in window alert for confirmYesOrNO Function
+    generatePassword();
+  }
+
+   // Character Type Section -Special Characters
+   var specialCharacterInput = window.prompt("Would you like to have special characters in your password? (Y/N)");
+   var yesOrNo = confirmYesOrNo(specialCharacterInput);
+   if (yesOrNo === true) {
+     // user wants special characters
+     pwSpecialCharactersResult = true;
+   } else if (yesOrNo === false) {
+     // user does not want special characters
+     pwSpecialCharactersResult = false;
+   } else {
+     // user did not choose y/n -prompted with message in window alert for confirmYesOrNO Function
+     generatePassword();
+   }
+
+   // Character Type Section -Numerical 
+   var numericalInput = window.prompt("Would you like to have numbers in your password? (Y/N)");
+   var yesOrNo = confirmYesOrNo(numericalInput);
+   if (yesOrNo === true) {
+     // user wants numbers
+     pwNumericalResult = true;
+   } else if (yesOrNo === false) {
+     // user does not want numbers
+     pwNumericalResult = false;
+   } else {
+     // user did not choose y/n -prompted with message in window alert for confirmYesOrNO Function
+     generatePassword();
+   }
 }
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// helper function
+// helper Functions
 function containsOnlyNumbers(str) {
   return /^\d+$/.test(str);
 }
