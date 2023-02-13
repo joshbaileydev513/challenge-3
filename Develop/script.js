ed02efce1bb11e02bef7eec7e6c0e84f37ddc93e
“[firstname <[joshbaileydev513@gmail.com]>
2023-02-13 17:54:58 -0500

    got the function written to prompt for password length and if they want lowercase characters. Also added helper functions

--- Develop/script.js
+++ Develop/script.js
@@ -7,12 +7,53 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+function generatePassword() {
+  // Password length section
+  var pwLength = window.prompt("Please choose a password length between 8-128 characters").toLowerCase(); 
+  
+  // Looping through user input-making sure only numeric values between 8-128 are present
+  if (containsOnlyNumbers(pwLength)){
+    // success
+    // user entered numbers- now checking pwLength is between 8-128 
+    pwLength = parseInt(pwLength)
+    console.log(pwLength)
+    if (pwLength >= 8 && pwLength <= 128) {
+      // user has entered an acceptable password length between 8-128
+    } else {
+      window.alert("Please enter numerical values between 8-128")
+    }
+  } else {
+    window.alert("Please enter numerical values only")
+  }
+
+  // Character Type Section -lowercase,UPPERCASE,numeric, special characters
+  var lowercaseInput = window.prompt("Would you like to have lowercase characters in your password? (Y/N)");
+  if (confirmYesOrNo(lowercaseInput)) {
+    // user wants lowercase
+  }
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// helper function
+function containsOnlyNumbers(str) {
+  return /^\d+$/.test(str);
+}
+
+function confirmYesOrNo(str) {
+  str = str.toLowerCase();
+  if (str === "y") {
+    return true
+  } else if (str === "n") {
+    return false
+  } else {
+    window.alert("Please only enter Y or N")
+  }
+}
 
 // click generate -> presented w/ series of prompts -> 1. password length (8-128) 2. character types -> 
 // A. lowercase B. uppercase C. numeric D. Special Characters (need 1 of each)
